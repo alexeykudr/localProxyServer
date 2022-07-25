d86a721887fdcac9ec3c20e9680880210e6e26d5
aliaksei-kudravets <frume28@gmail.com>
2022-07-25 17:24:28 +0200

    commit

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def rebootPort():
         if id:
             print('Modem reloading by url {}'.format(id))
             ip1, ip2 = router_api.rebootRouter(id)
-            return {"Ip is change!": f"Before {ip1} , after {ip2}"}
+            return {"Ip from to" : {ip1, ip2}}
 
         return {"Message": "Error"}
 
