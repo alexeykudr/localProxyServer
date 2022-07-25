5fe90379803f0d6ef94a1c9d9ba9da032de48946
aliaksei-kudravets <frume28@gmail.com>
2022-07-25 15:14:31 +0200

    :tmp

--- proxyapi.py
+++ proxyapi.py
@@ -23,7 +23,7 @@ class Configurator():
         "auth iponly strong\n",
         "users mama:CL:stiflera\n",
         f"users {self.user_login}:CL:{self.user_password}\n",
-        f"allow apple,{self.user_login}\n",
+        f"allow mama,{self.user_login}\n",
         "allow * 8.8.8.8,2.2.2.2 * * * * * \n",
         f"proxy -n -a -p70{self.portId-10} -i192.168.0.167 -e192.168.{self.portId}.100\n",
         f"socks -n -a -p80{self.portId-10} -i192.168.0.167 -e192.168.{self.portId}.100\n",
@@ -39,37 +39,27 @@ class Configurator():
         with open (file_path, 'w+') as example_conf:
             for line in self.config:
                 example_conf.writelines(line)
-        
-        result = subprocess.run(["sudo /bin/bash", "/home/{}/reload_service.sh".format(self.user_login),
-                                 "{}".format(id)],
-                                timeout=5, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        ult = os.system("/home/mac/reload_service.sh")
+        #result = subprocess.run(["sudo /bin/bash", "/home/{}/reload_service.sh".format(self.user_login),
+         #                        "{}".format(id)],
+          #                      timeout=5, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 class ProxyApi():
     # Api to reboot router by reload sh script, add job to cron which give reloading every N min.
     def __init__(self) -> None:
         self.user_login = os.environ["USER"]
         self.cron = CronTab(user=self.user_login)
 
-        # todo read tech proxies from file
-        
-        self.tech_proxy = dict()
-        
-        for i in range(1, 100):
-            self.tech_proxy[i+10] = f'46.227.245.119:700{i}:mama:stiflera'
-            # print(f'{i+10}:46.227.245.119:700{i}:mama:stiflera')
-        
-
-        
-        
-        
     def rebootRouter(self, id):
         # todo
         # want get ip addr bellow reload , it will be string
         # response_dict = dict()
         # response_dict["current_ip"] = self.getRouterIp(id)
-        # id = 25 "46.227.245.119:7015:mama:stifler",
-        proxy = self.tech_proxy[int(id)]
-        print(proxy)
-        result = os.system(f"sudo /home/{self.user_login}/reload.sh {id}")
+        ult = os.system(f"/home/{self.user_login}/reload.sh {id}")
+    #    result = subprocess.run(["sudo /bin/bash", "/home/{}/reload.sh".format(self.user_login),
+     #                            "{}".format(id)],
+      #                          timeout=25, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # response_dict["ip after change"] = self.getRouterIp(id)
+        # print(response_dict)
         
         
     def newJob(self, portId: int, interval: int) -> None:
@@ -99,8 +89,30 @@ class ProxyApi():
         result_str = ''.join(random.choice(letters) for i in range(length))
         return result_str
 
+    def getRouterIp(self, portId: str):
+        formated = [portId.split(":")]
+        proxy_data = formated[0]
+        proxies = {'https': 'http://{}:{}@{}:{}'.format(proxy_data[2],
+                                                        proxy_data[3],
+                                                        proxy_data[0],
+                                                        proxy_data[1])}
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET '
+            'CLR 3.5.30729)'}
+        with open('file.txt', 'a') as file:
+            try:
+                ipinfo_response = requests.get("https://ipinfo.io/ip",
+                                               headers=headers, proxies=proxies, timeout=2)
+                file.writelines(f'{ipinfo_response.text}\n')
+                file.close()
+            except Exception as e:
+                ipinfo_response = requests.get("https://ipinfo.io/ip",
+                                               headers=headers, proxies=proxies, timeout=2)
+                file.writelines(f'{ipinfo_response.text}\n')
+                file.close()
+        print(ipinfo_response.text)
+        return ipinfo_response.text
+
     def createProxyConfig(self, portId, user_log, user_pass):
         config = Configurator(portId, user_log, user_pass)
         config.writeConfig()
-
-    
